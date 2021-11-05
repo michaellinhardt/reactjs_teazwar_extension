3e119b528a90c32cbfb8e3c3778273f569bb6cd7
Michael Linhardt <linhardt.michael@gmail.com>
2021-11-05 15:54:23 +0700

    adding character generator ressources

---
+++ src/data/generator/index.js
@@ -0,0 +1,33 @@
+import ressources from './ressources.generator.js'
+
+const data = {
+  ressources,
+  listPart: [
+    'accA',
+    'accB',
+    'beastEars',
+    'body',
+    'cloak',
+    'clothing',
+    'ears',
+    'eyebrows',
+    'eyes',
+    'face',
+    'facialMark',
+    'frontHair',
+    'glasses',
+    'mouth',
+    'nose',
+    'rearHair',
+    'tail',
+    'wing',
+    'beard',
+  ],
+  listGender: [
+    'f',
+    'k',
+    'm',
+  ],
+}
+
+export default data
